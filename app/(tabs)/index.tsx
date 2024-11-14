39eb255cbcf6685030b365cb911313f414e013bb
Umut Topuzoğlu <uloco@schiefewelt.de>
2024-11-14 17:09:47 +0100

    Add PlaceholderImage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,15 @@
 import { Text, View, StyleSheet } from "react-native";
 import { Link } from "expo-router";
+import { Image } from "expo-image";
+
+const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
   return (
     <View style={styles.container}>
+      <View style={styles.imageContainer}>
+      <Image source={PlaceholderImage} style={styles.image} />
+      </View>
       <Text style={styles.text}>Home screen</Text>
       <Link href="/about" style={styles.button}>
         Go to About screen
@@ -26,5 +32,13 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textDecorationLine: "underline",
     color: "#fff",
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 });
